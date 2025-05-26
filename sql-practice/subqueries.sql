f6045b1c805942f53dfee776238ca348d861a0dd
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2025-05-26 22:12:24 +0100

    subqueries

---
+++ sql-practice/subqueries.sql
@@ -0,0 +1,25 @@
+SELECT toys.name AS toy FROM toys
+JOIN cats ON (cats.id = toys.cat_id)
+WHERE cats.name = 'Garfield';
+
+SELECT name FROM toys
+WHERE cat_id IN (SELECT id FROM cats WHERE name = 'Garfield');
+
+INSERT INTO toys (name, cat_id)
+VALUES (
+    'Pepperoni',
+    (
+        SELECT id FROM cats WHERE name = 'Garfield'
+    )
+);
+
+INSERT INTO toys (name, cat_id)
+SELECT 'Cat Bed', id
+FROM cats 
+WHERE birth_year < 2013;
+
+INSERT INTO cats_backup
+SELECT * FROM cats;
+
+INSERT INTO toys_backup
+SELECT * FROM toys;
